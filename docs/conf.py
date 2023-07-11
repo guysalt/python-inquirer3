3b2c59eca8e1ef0cdcacf8519097cd720b49d124
Guy Salton <guy123121@gmail.com>
2023-07-12 01:23:11 +0300

    docs/conf: remove old work around for bug

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,3 @@ extensions = [
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
-# workaround for bug: https://github.com/sphinx-doc/sphinx/issues/9383
-linkcheck_ignore = [
-    "codeofconduct.html",
-    "contributing.html",
-    "examples.html",
-]
