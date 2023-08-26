6e4c70742932c111324f3c1a5ad18ceab75fedd1
Guy Salton <guy123121@gmail.com>
2023-08-26 18:24:06 +0300

    tests: make acceptance/test_checkbox compatible for mac

--- tests/acceptance/test_checkbox.py
+++ tests/acceptance/test_checkbox.py
@@ -8,170 +8,178 @@ from readchar import key
 from inquirer3.themes import Default as Theme
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+def send_key(child, eof_string: str, key_to_press: str, times: int = 1):
+    for _ in range(times):
+        child.expect(eof_string, timeout=1)
+        child.send(key_to_press)
+
+
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class CheckTest(unittest.TestCase):
+    EOF_STRING = "History.*"
+
+    def send_key(self, key_to_press: str, times: int = 1):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times)
+
     def setUp(self):
         self.sut = pexpect.spawn("python examples/checkbox.py")
-        self.sut.expect("History.*", timeout=1)
 
     def test_default_input(self):
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books'\]}.*", timeout=1)  # noqa
 
     def test_select_the_third(self):
-        self.sut.send(key.DOWN)
-        self.sut.send(key.DOWN)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=2)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'Science'\]}.*", timeout=1)  # noqa
 
     def test_select_one_more(self):
-        self.sut.send(key.DOWN)
-        self.sut.send(key.DOWN)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.DOWN)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=2)
+        self.send_key(key.SPACE)
+        self.send_key(key.DOWN)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'Science', 'Nature'\]}.*", timeout=1)  # noqa
 
     def test_unselect(self):
-        self.sut.send(key.SPACE)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.SPACE)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Books', 'Computers'\]}.*", timeout=1)  # noqa
 
     def test_select_with_arrows(self):
-        self.sut.send(key.DOWN)
-        self.sut.send(key.DOWN)
-        self.sut.send(key.RIGHT)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=2)
+        self.send_key(key.RIGHT)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'Science'\]}.*", timeout=1)  # noqa
 
     def test_unselect_with_arrows(self):
-        self.sut.send(key.DOWN)
-        self.sut.send(key.LEFT)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN)
+        self.send_key(key.LEFT)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers'\]}.*", timeout=1)  # noqa
 
     def test_select_last(self):
-        for i in range(10):
-            self.sut.send(key.DOWN)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=10)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'History'\]}.*", timeout=1)  # noqa
 
     def test_select_all_with_ctrl_a(self):
-        self.sut.send(key.CTRL_A)
-        self.sut.send(key.ENTER)
+        self.send_key(key.CTRL_A)
+        self.send_key(key.ENTER)
         self.sut.expect(
             r"{'interests': \['Computers', 'Books', 'Science', 'Nature', 'Fantasy', 'History'\]}.*", timeout=1
         )  # noqa
 
     def test_reset_with_ctrl_r(self):
-        self.sut.send(key.CTRL_R)
-        self.sut.send(key.ENTER)
+        self.send_key(key.CTRL_R)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \[\]}.*", timeout=1)  # noqa
 
     def test_default_invert_selection_with_ctrl_i(self):
-        self.sut.send(key.CTRL_I)
-        self.sut.send(key.ENTER)
+        self.send_key(key.CTRL_I)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Science', 'Nature', 'Fantasy', 'History'\]}.*", timeout=1)  # noqa
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class CheckCarouselTest(unittest.TestCase):
+    EOF_STRING = "History.*"
+
+    def send_key(self, key_to_press: str, times: int = 1):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times)
+
     def setUp(self):
         self.sut = pexpect.spawn("python examples/checkbox_carousel.py")
-        self.sut.expect("Computers.*", timeout=1)
 
     def test_out_of_bounds_up(self):
-        self.sut.send(key.UP)
-        self.sut.expect("History.*", timeout=1)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.UP)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'History'\]}.*", timeout=1)  # noqa
 
     def test_out_of_bounds_down(self):
-        for i in range(6):
-            self.sut.send(key.DOWN)
-            # Not looking at what we expect along the way,
-            # let the last "expect" check that we got the right result
-            self.sut.expect(">.*", timeout=1)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=6)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Books'\]}.*", timeout=1)  # noqa
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class CheckOtherTest(unittest.TestCase):
+    EOF_STRING = r"\+ Other.*"
+
+    def send_key(self, key_to_press: str, times: int = 1):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times)
+
     def setUp(self):
         self.theme = Theme()
         self.sut = pexpect.spawn("python examples/checkbox_other.py")
-        self.sut.expect("Computers.*", timeout=1)
 
     def test_other_input(self):
-        self.sut.send(key.UP)
-        self.sut.expect(r"\+ Other.*", timeout=1)
-        self.sut.send(key.SPACE)
-        self.sut.expect(r": ", timeout=1)
+        self.send_key(key.UP)
+        self.send_key(key.SPACE)
+        self.sut.expect(":.*", timeout=1)
         self.sut.send("Hello world")
-        self.sut.expect(r"Hello world.*", timeout=1)
+        self.sut.expect("Hello world.*", timeout=1)
         self.sut.send(key.ENTER)
-        self.sut.expect(rf"> {escape(self.theme.Checkbox.selected_icon)} Hello world[\s\S]*\+ Other.*", timeout=1)
+        self.sut.expect(rf"> {escape(self.theme.Checkbox.selected_icon)} Hello world[\s\S]*\+ Other.*", timeout=3)
         self.sut.send(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books', 'Hello world'\]}", timeout=1)  # noqa
 
     def test_other_blank_input(self):
-        self.sut.send(key.UP)
-        self.sut.expect(r"\+ Other.*", timeout=1)
-        self.sut.send(key.SPACE)
-        self.sut.expect(r": ", timeout=1)
+        self.send_key(key.UP)
+        self.send_key(key.SPACE)
+        self.sut.expect(":.*", timeout=1)
         self.sut.send(key.ENTER)  # blank input
-        self.sut.expect(r"> \+ Other.*", timeout=1)
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Computers', 'Books'\]}", timeout=1)  # noqa
 
     def test_other_select_choice(self):
-        self.sut.send(key.SPACE)
-        self.sut.expect(rf"{escape(self.theme.Checkbox.unselected_icon)} Computers.*", timeout=1)
-        self.sut.send(key.ENTER)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'interests': \['Books'\]}", timeout=1)  # noqa
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class CheckWithTaggedValuesTest(unittest.TestCase):
     def setUp(self):
         self.sut = pexpect.spawn("python examples/checkbox_tagged.py")
-        self.sut.expect("History.*", timeout=1)
 
     def test_default_selection(self):
+        self.sut.expect("History.*", timeout=1)
         self.sut.send(key.ENTER)
-        self.sut.expect("{'interests': ", timeout=1)
+        self.sut.expect(r"{'interests': \['c', 'b'\]}", timeout=1)
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class CheckLockedTest(unittest.TestCase):
+    EOF_STRING = "DevOps.*"
+
+    def send_key(self, key_to_press: str, times: int = 1):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times)
+
     def setUp(self):
         self.sut = pexpect.spawn("python examples/checkbox_locked.py")
-        self.sut.expect("DevOps.*", timeout=1)
 
     def test_default_selection(self):
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'courses': \['Programming fundamentals'\]}", timeout=1)
 
     def test_locked_option_space(self):
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'courses': \['Programming fundamentals'\]}", timeout=1)
 
     def test_locked_option_left_key(self):
-        self.sut.send(key.LEFT)
-        self.sut.send(key.ENTER)
+        self.send_key(key.LEFT)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'courses': \['Programming fundamentals'\]}", timeout=1)
 
     def test_locked_with_another_option(self):
-        self.sut.send(key.DOWN)
-        self.sut.send(key.DOWN)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.DOWN, times=2)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
         self.sut.expect(r"{'courses': \['Programming fundamentals', 'Data science'\]}", timeout=1)
