db452c115cdd6c8c345eed6144abfb3c3d07993b
Guy Salton <guy123121@gmail.com>
2023-08-27 04:08:58 +0300

    tests: make acceptance/test_pre_answers compatible for mac

--- tests/acceptance/test_pre_answers.py
+++ tests/acceptance/test_pre_answers.py
@@ -1,66 +1,57 @@
 import sys
 import unittest
+from typing import Union
 
 import pexpect
 from readchar import key
 
+from tests.acceptance.utils import send_key
 
-expected_result = r"""\
-{'correct': True,\r\n
- 'organization': '',\r\n
- 'password': 'edcba',\r\n
- 'repo': 'default',\r\n
- 'topics': \['common'\],\r\n
- 'user': 'abcde'}\r\n"""
+expected_result = (
+    r"{'correct': True,\r\n "
+    r"'organization': '',\r\n "
+    r"'password': 'edcba',\r\n "
+    r"'repo': 'default',\r\n "
+    r"'topics': \['common'\],\r\n "
+    r"'user': 'abcde'}\r\n"
+)
 
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class PreAnswersTest(unittest.TestCase):
+    EOF_STRING = ":.*"
+
+    def send_key(self, key_to_press: str, times: int = 1, sleep_time: Union[float, int] = 0.2):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times, sleep_time=sleep_time)
+
     def setUp(self):
         self.sut = pexpect.spawn("python examples/pre_answers.py")
 
     def test_minimal_input(self):
         # user
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send("abcde")
+        self.send_key("abcde")
+        self.sut.expect("abcde", timeout=1)
         self.sut.send(key.ENTER)
+
         # password
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send("edcba")
+        self.send_key("edcba")
+        self.sut.expect(r"\*{5}", timeout=1)
         self.sut.send(key.ENTER)
+
         # repo
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER)
+
         # topics
-        self.sut.expect("Please define", timeout=1)
-        self.sut.send(key.SPACE)
-        self.sut.send(key.ENTER)
+        self.send_key(key.SPACE)
+        self.send_key(key.ENTER)
+
         # organization
-        self.sut.expect("If this is", timeout=1)
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER)
+
         # correct
-        self.sut.expect("This will delete", timeout=1)
-        self.sut.send("y")
+        self.send_key("y")
 
         # again
-
-        # user
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send(key.ENTER)
-        # password
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send(key.ENTER)
-        # repo
-        self.sut.expect("Please enter", timeout=1)
-        self.sut.send(key.ENTER)
-        # topics
-        self.sut.expect("Please define", timeout=1)
-        self.sut.send(key.ENTER)
-        # organization
-        self.sut.expect("If this is", timeout=1)
-        self.sut.send(key.ENTER)
-        # correct
-        self.sut.expect("This will delete", timeout=1)
-        self.sut.send(key.ENTER)
+        self.send_key(key.ENTER, times=6)
 
         self.sut.expect([expected_result, pexpect.EOF], timeout=1)
