5dc298dd16bbe9e415c01df3db335da914c031c3
Guy Salton <guy123121@gmail.com>
2023-08-27 11:28:04 +0300

    tests: make acceptance/test_shortcuts compatible for mac

--- tests/acceptance/test_shortcuts.py
+++ tests/acceptance/test_shortcuts.py
@@ -1,39 +1,43 @@
 import sys
+import time
 import unittest
 
 import pexpect
 from readchar import key
 
+from tests.acceptance.utils import send_key
 
-@unittest.skipUnless(sys.platform.startswith("lin"), "Linux only")
+
+@unittest.skipIf(sys.platform.startswith("win"), "Without Windows")
 class ShortcutsTest(unittest.TestCase):
+    EOF_STRING = ":.*"
+
+    def send_key(self, key_to_press: str, times: int = 1):
+        send_key(self.sut, self.EOF_STRING, key_to_press, times=times)
+
     def setUp(self):
         self.sut = pexpect.spawn("python examples/shortcuts.py")
 
-    def set_username(self, name="foo"):
-        self.sut.expect("Enter your username", timeout=1)
-        self.sut.sendline(name)
-
-    def set_password(self, password="secret"):  # nosec
-        self.sut.expect("Please enter your password", timeout=1)
-        self.sut.sendline(password)
-
-    def set_checkbox(self):
-        self.sut.expect("Please define your type of project", timeout=1)
-        self.sut.send(key.ENTER)
-
-    def set_list(self):
-        self.sut.expect("Public or private?", timeout=1)
-        self.sut.send(key.ENTER)
-
-    def set_confirm(self):
-        self.sut.expect("This will delete", timeout=1)
-        self.sut.send("y")
-        self.sut.send(key.ENTER)
-
     def test_shortcuts(self):
-        self.set_username()
-        self.set_password()
-        self.set_checkbox()
-        self.set_list()
-        self.set_confirm()
+        # username
+        self.send_key("foo")
+        self.sut.expect("foo")
+        self.sut.send(key.ENTER)
+
+        # password
+        self.send_key("secret")
+        self.sut.expect(r"\*{6}", timeout=1)
+        self.sut.send(key.ENTER)
+
+        # checkbox
+        self.sut.expect("frontend.*", timeout=1)
+        self.sut.send(key.ENTER)
+        time.sleep(0.5)
+
+        # list
+        self.sut.expect("private.*", timeout=1)
+        self.sut.send(key.ENTER)
+
+        # confirm
+        self.send_key("y")
+        self.sut.expect("True", timeout=1)
