f7d384d057ad8a0cd619af2ae46d7d1b95a838e9
Guy Salton <guy123121@gmail.com>
2023-09-02 16:20:07 +0300

    shortcuts: add all necessary parameters to the functions

--- src/inquirer3/shortcuts.py
+++ src/inquirer3/shortcuts.py
@@ -1,10 +1,13 @@
+from typing import Any
+
 import inquirer3.questions as questions
 from inquirer3.render.console import ConsoleRender
 
 
-def text(message, autocomplete=None, render=None, **kwargs):
+def text(message, default=None, autocomplete=None, trim_header=True, render=None, **kwargs):
     render = render or ConsoleRender()
-    question = questions.Text(name="", message=message, autocomplete=autocomplete, **kwargs)
+    question = questions.Text(name="", message=message, default=default, autocomplete=autocomplete,
+                              trim_header=trim_header, **kwargs)
     return render.render(question)
 
 
@@ -14,31 +17,41 @@ def editor(message, render=None, **kwargs):
     return render.render(question)
 
 
-def password(message, render=None, **kwargs):
+def password(message, default=None, autocomplete=None, trim_header=True, render=None,
+             **kwargs):
     render = render or ConsoleRender()
-    question = questions.Password(name="", message=message, **kwargs)
+    question = questions.Password(name="", message=message, default=default, autocomplete=autocomplete,
+                                  trim_header=trim_header, **kwargs)
     return render.render(question)
 
 
-def confirm(message, render=None, **kwargs):
+def confirm(message, default=None, trim_header=True, render=None, **kwargs):
     render = render or ConsoleRender()
-    question = questions.Confirm(name="", message=message, **kwargs)
+    question = questions.Confirm(name="", message=message, default=default, trim_header=trim_header, **kwargs)
     return render.render(question)
 
 
-def list_input(message, render=None, **kwargs):
+def list_input(message, choices=None, default: Any = None, ignore=False, validate=True, carousel=False, other=False,
+               autocomplete=None, trim_header=True, trim_choices=False, render=None):
     render = render or ConsoleRender()
-    question = questions.List(name="", message=message, **kwargs)
+    question = questions.List(name="", message=message, choices=choices, default=default, ignore=ignore,
+                              validate=validate, carousel=carousel, other=other, autocomplete=autocomplete,
+                              trim_header=trim_header, trim_choices=trim_choices)
     return render.render(question)
 
 
-def checkbox(message, render=None, **kwargs):
+def checkbox(message, choices=None, locked=None, default: Any = None, ignore=False, validate=True, carousel=False,
+             other=False, autocomplete=None, trim_header=True, trim_choices=False, render=None):
     render = render or ConsoleRender()
-    question = questions.Checkbox(name="", message=message, **kwargs)
+    question = questions.Checkbox(name="", message=message, choices=choices, locked=locked, default=default,
+                                  ignore=ignore, validate=validate, carousel=carousel, other=other,
+                                  autocomplete=autocomplete, trim_header=trim_header, trim_choices=trim_choices)
     return render.render(question)
 
 
-def path(message, render=None, **kwargs):
+def path(message, default: Any = None, trim_header=True, path_type="any", exists=None,
+         normalize_to_absolute_path=False, render=None, **kwargs):
     render = render or ConsoleRender()
-    question = questions.Path(name="", message=message, **kwargs)
+    question = questions.Path(name="", message=message, default=default, trim_header=trim_header, path_type=path_type,
+                              exists=exists, normalize_to_absolute_path=normalize_to_absolute_path, **kwargs)
     return render.render(question)
