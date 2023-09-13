a205582da349f633a0df3ca835162117a41bc99c
Guy Salton <guy123121@gmail.com>
2023-09-13 23:33:01 +0300

    shortcuts: add all parameters to shorcuts

--- src/inquirer3/shortcuts.py
+++ src/inquirer3/shortcuts.py
@@ -7,35 +7,56 @@ from inquirer3.render.console import ConsoleRender
 def text(
     message: Any,
     default: Optional[str] = None,
-    autocomplete=None,
+    ignore: Union[bool, Callable] = False,
+    validate: Union[bool, Callable] = True,
+    show_default: Union[bool, Callable] = False,
     trim_header: bool = True,
+    autocomplete=None,
     render: Optional[ConsoleRender] = None,
-    **kwargs
+    raise_keyboard_interrupt: bool = False
 ) -> str:
-    render = render or ConsoleRender()
+    render = render or ConsoleRender(raise_keyboard_interrupt=raise_keyboard_interrupt)
     question = questions.Text(
-        name="", message=message, default=default, autocomplete=autocomplete, trim_header=trim_header, **kwargs
+        name="", message=message, default=default, ignore=ignore, validate=validate, show_default=show_default,
+        autocomplete=autocomplete, trim_header=trim_header,
     )
     return render.render(question)
 
 
-def editor(message: Any, render: Optional[ConsoleRender] = None, **kwargs) -> str:
-    render = render or ConsoleRender()
-    question = questions.Editor(name="", message=message, **kwargs)
+def editor(
+    message: Any,
+    default: Optional[str] = None,
+    ignore: Union[bool, Callable] = False,
+    validate: Union[bool, Callable] = True,
+    show_default: Union[bool, Callable] = False,
+    trim_header: bool = True,
+    autocomplete=None,
+    render: Optional[ConsoleRender] = None,
+    raise_keyboard_interrupt: bool = False
+) -> str:
+    render = render or ConsoleRender(raise_keyboard_interrupt=raise_keyboard_interrupt)
+    question = questions.Editor(
+        name="", message=message, default=default, ignore=ignore, validate=validate, show_default=show_default,
+        autocomplete=autocomplete, trim_header=trim_header,
+    )
     return render.render(question)
 
 
 def password(
     message: Any,
-    default: Optional[str] = None,
-    autocomplete=None,
-    trim_header: bool = True,
+    echo: str = "*",
+    default: Any = None,
+    ignore: Union[bool, Callable] = False,
+    validate: Union[bool, Callable] = True,
+    show_default: Union[bool, Callable] = False,
+    trim_header: Union[bool, Callable] = True,
+    autocomplete: Optional[Callable] = None,
     render: Optional[ConsoleRender] = None,
-    **kwargs
 ) -> str:
     render = render or ConsoleRender()
     question = questions.Password(
-        name="", message=message, default=default, autocomplete=autocomplete, trim_header=trim_header, **kwargs
+        name="", message=message, echo=echo, default=default, ignore=ignore, validate=validate,
+        show_default=show_default, trim_header=trim_header, autocomplete=autocomplete,
     )
     return render.render(question)
 
